41df1c3e0362a12aaa4cf6648bdc1b6ceb292519
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-01-31 09:05:04 +0200

    Add files via upload

---
+++ Constructors/ConsoleApp5/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Car
+    {
+        public string _brand;
+        public string _model;
+        public int _km;
+
+        public Car(int km)
+        {
+            _km = km;
+        }
+
+        public Car(string brand, string model, int km = 0)
+        {
+            _brand = brand;
+            _model = model;
+            _km = km;
+        }
+
+        public int TellMeTheKM()
+        {
+            return _km;
+        }
+
+        public override string ToString()
+        {
+            return $"Car Brand : {_brand} Model : {_model} KM : {_km}";
+        }
+
+    }
+}
---
+++ Constructors/ConsoleApp5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // IL dasm .exe
+
+            //Car mit = new Car
+            //{
+            //    _brand = "Mitsubishi",
+            //    _model = "Outlander",
+            //    _km = 100
+            //};
+
+            Car honda = new Car("Mitsubishi", "Outlander", 100); // calls the constructor ctor
+
+            Console.WriteLine(honda);
+
+            Car ferrari = new Car(100)
+            {
+                _model = "ferrari"
+            };
+           
+            //honda._brand = "Honda";
+            //honda._model = "Kamry";
+            //honda._km = 1000;
+
+        }
+    }
+}
